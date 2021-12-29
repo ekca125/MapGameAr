b4e635785a30f4e6de97fa19f50c5d74ed4c4102
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-29 16:46:48 +0900

    Update ThisApplicationTest.java

--- app/src/androidTest/java/com/ekcapaper/racingar/data/ThisApplicationTest.java
+++ app/src/androidTest/java/com/ekcapaper/racingar/data/ThisApplicationTest.java
@@ -2,7 +2,11 @@ package com.ekcapaper.racingar.data;
 
 import static org.junit.Assert.*;
 
+import androidx.test.core.app.ApplicationProvider;
+
 import org.junit.Test;
+
+import java.util.function.Supplier;
 
 public class ThisApplicationTest {
 
@@ -11,8 +15,11 @@ public class ThisApplicationTest {
     }
 
     @Test
-    public void login() {
-        
-
+    public void login() throws Exception {
+        ThisApplication thisApplication = (ThisApplication) ApplicationProvider.getApplicationContext();
+        String email = "abcd@example.com";
+        String password = "abcdefg";
+        thisApplication.login(email, password);
+        thisApplication.getSession().orElseThrow(() -> new Exception("Login Error"));
     }
 }
