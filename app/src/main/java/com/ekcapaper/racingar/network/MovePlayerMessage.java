23afe412764bccb41178201901acec6451088f17
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-30 14:56:35 +0900

    ab

--- app/src/main/java/com/ekcapaper/racingar/network/MovePlayerMessage.java
+++ app/src/main/java/com/ekcapaper/racingar/network/MovePlayerMessage.java
@@ -1,14 +1,16 @@
 package com.ekcapaper.racingar.network;
 
+import com.google.gson.Gson;
+
 import lombok.Builder;
 
 public class MovePlayerMessage extends Message {
     private final String userId;
-    private final String latitude;
-    private final String longitude;
+    private final double latitude;
+    private final double longitude;
 
     @Builder
-    public MovePlayerMessage(String userId, String latitude, String longitude) {
+    public MovePlayerMessage(String userId, double latitude, double longitude) {
         super(OpCode.MOVE_PLAYER);
         this.userId = userId;
         this.latitude = latitude;
@@ -17,8 +19,7 @@ public class MovePlayerMessage extends Message {
 
     @Override
     public String getPayload() {
-
-
-        return null;
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 }
