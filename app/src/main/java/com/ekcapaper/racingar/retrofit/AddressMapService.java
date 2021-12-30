4a8f2595d0b3d14985e3be023815bac4e8467c04
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-30 20:33:32 +0900

    Feat : Retrofit2를 이용하여 서버에서 맵을 생성하여 가져오는 API를 호출하는 기능 추가 (#6)

---
+++ app/src/main/java/com/ekcapaper/racingar/retrofit/AddressMapService.java
@@ -0,0 +1,32 @@
+package com.ekcapaper.racingar.retrofit;
+
+import com.ekcapaper.racingar.retrofit.dto.AddressDto;
+import com.ekcapaper.racingar.retrofit.dto.MapRange;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface AddressMapService {
+    @GET("/api/v1/address/id={id}")
+    Call<AddressDto> findAddress(@Path("id") long id);
+
+    @GET("/api/v1/address/draw/random")
+    Call<AddressDto> drawRandom();
+
+    @POST("/api/v1/address/draw/range")
+    Call<List<AddressDto>> drawMapRangeRandom(@Body MapRange mapRange);
+
+    @POST("/api/v1/address/draw/range-limit-10")
+    Call<List<AddressDto>> drawMapRangeRandom10(@Body MapRange mapRange);
+
+    @POST("/api/v1/address/draw/range-limit-50")
+    Call<List<AddressDto>> drawMapRangeRandom50(@Body MapRange mapRange);
+
+    @POST("/api/v1/address/draw/range-limit-100")
+    Call<List<AddressDto>> drawMapRangeRandom100(@Body MapRange mapRange);
+}
---
+++ app/src/main/java/com/ekcapaper/racingar/retrofit/dto/AddressDto.java
@@ -0,0 +1,23 @@
+package com.ekcapaper.racingar.retrofit.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class AddressDto {
+    @SerializedName("id")
+    long id;
+    @SerializedName("latitude")
+    double latitude;
+    @SerializedName("longitude")
+    double longitude;
+
+    @Builder
+    public AddressDto(long id, double latitude, double longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
