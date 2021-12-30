1e3825c255b8db76bbb138648e67e93d92260a4f
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-30 20:51:00 +0900

    Update RoomOperatorFlagGame.java

--- app/src/main/java/com/ekcapaper/racingar/operator/RoomOperatorFlagGame.java
+++ app/src/main/java/com/ekcapaper/racingar/operator/RoomOperatorFlagGame.java
@@ -1,4 +1,26 @@
 package com.ekcapaper.racingar.operator;
 
+import com.heroiclabs.nakama.Match;
+import com.heroiclabs.nakama.Session;
+import com.heroiclabs.nakama.SocketClient;
+
 public class RoomOperatorFlagGame extends RoomOperator{
+    public RoomOperatorFlagGame(Session session, SocketClient socketClient, Match match) {
+        super(session, socketClient, match);
+    }
+
+    @Override
+    protected boolean isEnd() {
+        return false;
+    }
+
+    @Override
+    protected boolean isVictory() {
+        return false;
+    }
+
+    @Override
+    protected boolean isDefeat() {
+        return false;
+    }
 }
