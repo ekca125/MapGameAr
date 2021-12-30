cc7ba18b5f7cb20939116104a288a06ed6f26829
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-30 15:16:27 +0900

    Update RoomOperator.java

--- app/src/main/java/com/ekcapaper/racingar/operator/RoomOperator.java
+++ app/src/main/java/com/ekcapaper/racingar/operator/RoomOperator.java
@@ -1,4 +1,13 @@
 package com.ekcapaper.racingar.operator;
 
+import com.ekcapaper.racingar.game.Player;
+import com.heroiclabs.nakama.UserPresence;
+
+import java.util.List;
+
 public class RoomOperator {
+    List<UserPresence> userPresenceList;
+    List<Player> playerList;
+
+
 }
