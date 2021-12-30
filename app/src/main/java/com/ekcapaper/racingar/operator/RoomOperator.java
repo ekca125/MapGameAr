06acc288e4b209089667ad5b5a91f70d0ca0e4db
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-30 15:23:20 +0900

    Update RoomOperator.java

--- app/src/main/java/com/ekcapaper/racingar/operator/RoomOperator.java
+++ app/src/main/java/com/ekcapaper/racingar/operator/RoomOperator.java
@@ -1,13 +1,93 @@
 package com.ekcapaper.racingar.operator;
 
 import com.ekcapaper.racingar.game.Player;
+import com.heroiclabs.nakama.AbstractSocketListener;
+import com.heroiclabs.nakama.ChannelPresenceEvent;
+import com.heroiclabs.nakama.Error;
+import com.heroiclabs.nakama.Match;
+import com.heroiclabs.nakama.MatchData;
+import com.heroiclabs.nakama.MatchPresenceEvent;
+import com.heroiclabs.nakama.MatchmakerMatched;
+import com.heroiclabs.nakama.Session;
+import com.heroiclabs.nakama.SocketClient;
+import com.heroiclabs.nakama.StatusPresenceEvent;
+import com.heroiclabs.nakama.StreamData;
+import com.heroiclabs.nakama.StreamPresenceEvent;
 import com.heroiclabs.nakama.UserPresence;
+import com.heroiclabs.nakama.api.ChannelMessage;
+import com.heroiclabs.nakama.api.NotificationList;
 
 import java.util.List;
 
-public class RoomOperator {
+public class RoomOperator extends AbstractSocketListener{
+    // 서버에서의 유저들과 현재 방에서의 플레이어를 의미한다.
     List<UserPresence> userPresenceList;
     List<Player> playerList;
+    // 서버와의 연동
+    Session session;
+    SocketClient socketClient;
+    Match match;
 
+    public RoomOperator(Session session, SocketClient socketClient, Match match) {
+        this.session = session;
+        this.socketClient = socketClient;
+        this.match = match;
 
+        socketClient.connect(session,this);
+    }
+
+    @Override
+    public void onDisconnect(Throwable t) {
+        super.onDisconnect(t);
+    }
+
+    @Override
+    public void onError(Error error) {
+        super.onError(error);
+    }
+
+    @Override
+    public void onChannelMessage(ChannelMessage message) {
+        super.onChannelMessage(message);
+    }
+
+    @Override
+    public void onChannelPresence(ChannelPresenceEvent presence) {
+        super.onChannelPresence(presence);
+    }
+
+    @Override
+    public void onMatchmakerMatched(MatchmakerMatched matched) {
+        super.onMatchmakerMatched(matched);
+    }
+
+    @Override
+    public void onMatchData(MatchData matchData) {
+        super.onMatchData(matchData);
+    }
+
+    @Override
+    public void onMatchPresence(MatchPresenceEvent matchPresence) {
+        super.onMatchPresence(matchPresence);
+    }
+
+    @Override
+    public void onNotifications(NotificationList notifications) {
+        super.onNotifications(notifications);
+    }
+
+    @Override
+    public void onStatusPresence(StatusPresenceEvent presence) {
+        super.onStatusPresence(presence);
+    }
+
+    @Override
+    public void onStreamPresence(StreamPresenceEvent presence) {
+        super.onStreamPresence(presence);
+    }
+
+    @Override
+    public void onStreamData(StreamData data) {
+        super.onStreamData(data);
+    }
 }
