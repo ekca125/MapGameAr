75cab6a5bc9785a55486ddfcc019421638a1579f
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-31 10:23:48 +0900

    Update RoomOperatorFactory.java

--- app/src/main/java/com/ekcapaper/racingar/operator/RoomOperatorFactory.java
+++ app/src/main/java/com/ekcapaper/racingar/operator/RoomOperatorFactory.java
@@ -1,4 +1,21 @@
 package com.ekcapaper.racingar.operator;
 
+import com.ekcapaper.racingar.retrofit.AddressMapClient;
+import com.ekcapaper.racingar.retrofit.AddressMapService;
+import com.heroiclabs.nakama.Match;
+import com.heroiclabs.nakama.Session;
+import com.heroiclabs.nakama.SocketClient;
+
 public class RoomOperatorFactory {
+    public static RoomOperator createRoomOperator(String type, Session session, SocketClient socketClient, Match match){
+        if(type.equals("flagGame")){
+            AddressMapService addressMapService = AddressMapClient.getMapAddressService();
+            addressMapService.
+
+
+        }
+        else{
+            return null;
+        }
+    }
 }
