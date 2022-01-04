489749a7bc65d03cfa97fa2d1047237aa59fb1d4
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-04 11:57:26 +0900

    Update RoomLinker.java

--- app/src/main/java/com/ekcapaper/racingar/operator/RoomLinker.java
+++ app/src/main/java/com/ekcapaper/racingar/operator/RoomLinker.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.heroiclabs.nakama.AbstractSocketListener;
 import com.heroiclabs.nakama.Channel;
 import com.heroiclabs.nakama.ChannelPresenceEvent;
+import com.heroiclabs.nakama.ChannelType;
 import com.heroiclabs.nakama.Client;
 import com.heroiclabs.nakama.Error;
 import com.heroiclabs.nakama.Match;
@@ -28,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 import lombok.Getter;
 
@@ -36,20 +38,18 @@ public class RoomLinker extends AbstractSocketListener {
     private final Client client;
     private final Session session;
     private final SocketClient socketClient;
-    // 유저의 정보
-    private final List<UserPresence> realTimeUserPresenceList;
-    // 채팅 데이터
-    private final List<UserPresence> chatUserPresenceList;
-    private final List<String> chatLog;
-    // 매치 ID와 채팅 ID
-    private String matchId;
-    private String chatChannelId;
-    // 서버와 연동된 매치와 채팅
+    // 서버와의 연동을 의미하는 객체들(Realtime, Chat Channel)
     @Getter
-    private Optional<Match> currentMatch;
-    private Optional<Channel> currentChannel;
+    private Match currentMatch;
+    @Getter
+    private Channel currentChatChannel;
+    // 유저 프로필 (Realtime)
+    private final List<UserPresence> realTimeUserPresenceList;
+    // 유저 프로필 (Chat Channel)
+    private final List<UserPresence> chatUserPresenceList;
 
     public RoomLinker(Client client, Session session) throws ExecutionException, InterruptedException {
+        // 서버와의 연동에 필요한 객체
         this.client = client;
         this.session = session;
         this.socketClient = client.createSocket(
@@ -57,62 +57,63 @@ public class RoomLinker extends AbstractSocketListener {
                 KeyStorageNakama.getWebSocketPort(),
                 KeyStorageNakama.getWebSocketSSL()
         );
+        // 서버와의 연동을 의미하는 객체들(Realtime, Chat Channel)
+        this.currentMatch = null;
+        this.currentChatChannel = null;
         // 유저들의 프로필
         this.realTimeUserPresenceList = new ArrayList<>();
         // 유저들의 채팅 프로필과 채팅 데이터
         this.chatUserPresenceList = new ArrayList<>();
-        this.chatLog = new ArrayList<>();
         // 소켓 연결 및 매치 설정
         this.socketClient.connect(session, this).get();
-        this.currentMatch = Optional.empty();
     }
 
-    // 방 생성 또는 입장
-    public final void createMatch() {
+    public final boolean createMatch(){
         try {
-            this.currentMatch = Optional.ofNullable(this.socketClient.createMatch().get());
-
-
-        } catch (ExecutionException | InterruptedException e) {
-            currentMatch = Optional.empty();
+            this.currentMatch = socketClient.createMatch().get();
+            this.currentChatChannel = socketClient.joinChat(this.currentMatch.getMatchId(),ChannelType.ROOM).get();
+            return true;
+        }
+        catch (ExecutionException | InterruptedException e) {
+            this.currentMatch = null;
+            this.currentChatChannel = null;
+            return false;
         }
     }
 
-    public final void joinMatch(String matchId) {
+    public final boolean joinMatch(String matchId) {
         try {
-            currentMatch = Optional.ofNullable(this.socketClient.joinMatch(matchId).get());
+            this.currentMatch = socketClient.joinMatch(matchId).get();
+            this.currentChatChannel = socketClient.joinChat(this.currentMatch.getMatchId(),ChannelType.ROOM).get();
+            return true;
         } catch (ExecutionException | InterruptedException e) {
-            currentMatch = Optional.empty();
+            this.currentMatch = null;
+            this.currentChatChannel = null;
+            return false;
         }
     }
 
     // send Message
     public final void sendMatchData(Message message) {
-        currentMatch.ifPresent(match -> {
-            socketClient.sendMatchData(
-                    match.getMatchId(),
-                    message.getOpCode().ordinal(),
-                    message.getPayload().getBytes(StandardCharsets.UTF_8)
-            );
-        });
+        socketClient.sendMatchData(
+                currentMatch.getMatchId(),
+                message.getOpCode().ordinal(),
+                message.getPayload().getBytes(StandardCharsets.UTF_8)
+        );
     }
 
     // receive
     @Override
     public void onDisconnect(Throwable t) {
-        // 연결이 끊어졌을 때
+        // 연결이 끊어졌을 때에는 즉시 종료
         super.onDisconnect(t);
-        // 현재 매치 무효화
-        this.currentMatch.ifPresent((match)->{
-            String matchId = match.getMatchId();
-            this.socketClient.leaveMatch(matchId);
-            this.socketClient.leaveChat();
-        });
+        socketClient.leaveMatch(currentMatch.getMatchId());
+        socketClient.leaveChat(currentChatChannel.getId());
 
-        this.socketClient.leaveMatch();
+        currentMatch = null;
+        currentChatChannel = null;
 
-        this.currentMatch = Optional.empty();
-
+        this.socketClient.disconnect();
     }
 
     @Override
@@ -120,10 +121,25 @@ public class RoomLinker extends AbstractSocketListener {
         super.onError(error);
     }
 
+    /*
+        Code	Purpose	Source	Description
+    0	chat message	user	All messages sent by users.
+    1	chat update	user	A user updating a message they previously sent.
+    2	chat remove	user	A user removing a message they previously sent.
+    3	joined group	server	An event message for when a user joined the group.
+    4	added to group	server	An event message for when a user was added to the group.
+    5	left group	server	An event message for when a user left a group.
+    6	kicked from group	server	An event message for when an admin kicked a user from the group.
+    7	promoted in group	server	An event message for when a user is promoted as a group admin.
+    8	banned in group	server	An event message for when a user got banned from a group.
+    9	demoted in group	server	An event message for when a user got demoted in group.
+    */
     @Override
     public void onChannelMessage(ChannelMessage message) {
         super.onChannelMessage(message);
-        chatLog.add(message.getUsername() + " : " + message.getContent());
+        int messageCode = message.getCode().getValue();
+
+
     }
 
     @Override
