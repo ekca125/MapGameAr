007b6edf23173193c482524b9c2f8b9aaa543d6d
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-04 20:20:25 +0900

    Update GameRoomOperator.java

--- app/src/main/java/com/ekcapaper/racingar/operator/GameRoomOperator.java
+++ app/src/main/java/com/ekcapaper/racingar/operator/GameRoomOperator.java
@@ -1,14 +1,37 @@
 package com.ekcapaper.racingar.operator;
 
+import com.ekcapaper.racingar.network.GameEndMessage;
 import com.heroiclabs.nakama.Client;
 import com.heroiclabs.nakama.Session;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class GameRoomOperator extends GameRoomClient{
+    Timer endCheckTimer;
+    TimerTask endCheckTimerTask;
 
     public GameRoomOperator(Client client, Session session) {
         super(client, session);
+        endCheckTimer = new Timer();
+        endCheckTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if(isEnd()){
+                    sendMatchData(new GameEndMessage());
+                }
+            }
+        };
+        endCheckTimer.schedule(endCheckTimerTask,
+                TimeUnit.SECONDS.convert(1,TimeUnit.MILLISECONDS),
+                TimeUnit.SECONDS.convert(1,TimeUnit.MILLISECONDS)
+        );
     }
 
-    
+    public boolean isEnd(){
 
+    }
+
+    // end를 상위클래스에서 재정의
 }
