04e68c1046c1165f3042ca8a3df8bef8b0015d2b
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-06 10:24:00 +0900

    Create GameRoomClientMaker.java

---
+++ app/src/main/java/com/ekcapaper/racingar/operator/maker/GameRoomClientMaker.java
@@ -0,0 +1,7 @@
+package com.ekcapaper.racingar.operator.maker;
+
+import com.ekcapaper.racingar.operator.layer.GameRoomClient;
+
+public interface GameRoomClientMaker {
+    GameRoomClient makeGameRoomClient();
+}
