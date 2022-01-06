0a3058701c6c9b40d93bcd7442b18d9431705b2f
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-06 10:32:24 +0900

    commit

---
+++ app/src/main/java/com/ekcapaper/racingar/operator/maker/joinroom/GameRoomClientJoinMaker.java
@@ -0,0 +1,26 @@
+package com.ekcapaper.racingar.operator.maker.joinroom;
+
+import com.ekcapaper.racingar.operator.layer.GameRoomClient;
+import com.ekcapaper.racingar.operator.layer.RoomClient;
+import com.ekcapaper.racingar.operator.maker.GameRoomClientMaker;
+import com.heroiclabs.nakama.Client;
+import com.heroiclabs.nakama.Session;
+
+public class GameRoomClientJoinMaker implements GameRoomClientMaker {
+    private final Client client;
+    private final Session session;
+    private final String matchId;
+
+    public GameRoomClientJoinMaker(Client client, Session session, String matchId) {
+        this.client = client;
+        this.session = session;
+        this.matchId = matchId;
+    }
+
+    @Override
+    public GameRoomClient makeGameRoomClient() {
+        GameRoomClient gameRoomClient = new GameRoomClient(client,session);
+        gameRoomClient.createMatch();
+        return gameRoomClient;
+    }
+}
---
+++ app/src/main/java/com/ekcapaper/racingar/operator/maker/newroom/GameRoomClientNewMaker.java
@@ -0,0 +1,23 @@
+package com.ekcapaper.racingar.operator.maker.newroom;
+
+import com.ekcapaper.racingar.operator.layer.GameRoomClient;
+import com.ekcapaper.racingar.operator.maker.GameRoomClientMaker;
+import com.heroiclabs.nakama.Client;
+import com.heroiclabs.nakama.Session;
+
+public class GameRoomClientNewMaker implements GameRoomClientMaker {
+    private final Client client;
+    private final Session session;
+
+    public GameRoomClientNewMaker(Client client, Session session) {
+        this.client = client;
+        this.session = session;
+    }
+
+    @Override
+    public GameRoomClient makeGameRoomClient() {
+        GameRoomClient gameRoomClient = new GameRoomClient(client,session);
+        gameRoomClient.createMatch();
+        return gameRoomClient;
+    }
+}
