352b8e90f1e8798ec78814673277d214d428b639
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-10 15:08:58 +0900

    Update LoginActivity.java

--- app/src/main/java/com/ekcapaper/racingar/activity/raar/LoginActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/LoginActivity.java
@@ -3,14 +3,30 @@ package com.ekcapaper.racingar.activity.raar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.ekcapaper.racingar.R;
+import com.ekcapaper.racingar.utils.Tools;
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private View parent_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        parent_view = findViewById(android.R.id.content);
+
+        ((View) findViewById(R.id.sign_up)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(parent_view, "Sign Up", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        Tools.setSystemBarColor(this);
     }
 }
