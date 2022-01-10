c7b9fe09fccb7fbbc9669d7861a1e0cc11ce944b
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-10 15:07:31 +0900

    commit

---
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/GameRoomActivity.java
@@ -0,0 +1,14 @@
+package com.ekcapaper.racingar.activity.raar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.ekcapaper.racingar.R;
+public class GameRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_room);
+    }
+}
