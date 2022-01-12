846cf6a2dcf581908be1a6d5da83a033374f8561
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-12 15:03:30 +0900

    delete

--- app/src/main/java/com/ekcapaper/racingar/operator/maker/make/FlagGameRoomOperatorMaker.java
+++
@@ -1,7 +0,0 @@
-package com.ekcapaper.racingar.operator.maker.make;
-
-import com.ekcapaper.racingar.operator.impl.FlagGameRoomOperator;
-
-public interface FlagGameRoomOperatorMaker {
-    FlagGameRoomOperator makeFlagGameRoomOperator();
-}
--- app/src/main/java/com/ekcapaper/racingar/operator/maker/make/TimeLimitGameRoomOperatorMaker.java
+++
@@ -1,7 +0,0 @@
-package com.ekcapaper.racingar.operator.maker.make;
-
-import com.ekcapaper.racingar.operator.layer.TimeLimitGameRoomOperator;
-
-public interface TimeLimitGameRoomOperatorMaker {
-    TimeLimitGameRoomOperator makeTimeLimitGameRoomOperator();
-}
