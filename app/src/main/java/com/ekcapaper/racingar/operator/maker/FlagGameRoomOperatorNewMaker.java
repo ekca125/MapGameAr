d914a3e3192b3120b43679ed264c26209fcef86d
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-12 15:47:59 +0900

    commit

---
+++ app/src/main/java/com/ekcapaper/racingar/operator/maker/FlagGameRoomOperatorNewMaker.java
@@ -0,0 +1,12 @@
+package com.ekcapaper.racingar.operator.maker;
+
+import com.ekcapaper.racingar.operator.layer.GameRoomOperator;
+
+public class FlagGameRoomOperatorNewMaker implements GameRoomOperatorMaker{
+
+
+    @Override
+    public GameRoomOperator make() {
+        return null;
+    }
+}
