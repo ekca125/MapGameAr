43c483520f5c0728c9b257944b69af8899f8786a
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-12 15:12:39 +0900

    Update RoomInfoReader.java

--- app/src/main/java/com/ekcapaper/racingar/modelgame/gameroom/reader/RoomInfoReader.java
+++ app/src/main/java/com/ekcapaper/racingar/modelgame/gameroom/reader/RoomInfoReader.java
@@ -1,4 +1,9 @@
 package com.ekcapaper.racingar.modelgame.gameroom.reader;
 
+import com.ekcapaper.racingar.modelgame.gameroom.info.RoomInfo;
+
 public class RoomInfoReader {
+    static public RoomInfo getRoomInfo(String matchId){
+        
+    }
 }
