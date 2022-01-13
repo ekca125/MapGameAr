3cb36d3e5e672d6f92f4491e0b51b122079baf19
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-13 10:47:50 +0900

    commit

--- app/src/main/java/com/ekcapaper/racingar/activity/raar/SetupAppActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/SetupAppActivity.java
@@ -3,14 +3,24 @@ package com.ekcapaper.racingar.activity.raar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ekcapaper.racingar.R;
 
 public class SetupAppActivity extends AppCompatActivity {
 
+    Button button_request_permission;
+    TextView textView_setup_app;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_app);
+
+        button_request_permission = findViewById(R.id.button_request_permission);
+        textView_setup_app = findViewById(R.id.textView_setup_app);
+
+
     }
 }
