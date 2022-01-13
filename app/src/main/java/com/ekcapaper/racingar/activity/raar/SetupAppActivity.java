51a891b02f9082385517388a4067ea488f9a9dfc
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-13 11:21:51 +0900

    commit

--- app/src/main/java/com/ekcapaper/racingar/activity/raar/SetupAppActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/SetupAppActivity.java
@@ -1,14 +1,25 @@
 package com.ekcapaper.racingar.activity.raar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.ekcapaper.racingar.R;
 
 public class SetupAppActivity extends AppCompatActivity {
+    private final int PERMISSION_REQUEST_CODE = 1;
+    private final int ACTIVITY_REQUEST_CODE = 2;
 
     Button button_request_permission;
     TextView textView_setup_app;
@@ -21,6 +32,42 @@ public class SetupAppActivity extends AppCompatActivity {
         button_request_permission = findViewById(R.id.button_request_permission);
         textView_setup_app = findViewById(R.id.textView_setup_app);
 
+        button_request_permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                permissionCheckAndAction();
+            }
+        });
+        permissionCheckAndAction();
+    }
 
+    private void startNextActivity(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivityForResult(intent,ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        finish();
+    }
+
+    protected void permissionCheckAndAction(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            startNextActivity();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startNextActivity();
+            }
+        }
     }
 }
