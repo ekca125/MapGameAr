8b20659d65831258cc02db16365164d9776a94c3
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-13 10:27:34 +0900

    setup app activity

---
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/SetupAppActivity.java
@@ -0,0 +1,16 @@
+package com.ekcapaper.racingar.activity.raar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ekcapaper.racingar.R;
+
+public class SetupAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_app);
+    }
+}
