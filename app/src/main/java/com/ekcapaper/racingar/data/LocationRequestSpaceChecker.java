48318a6d384b3de1bc97d1ce7604fc6a638995c9
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-14 15:10:03 +0900

    Create LocationRequestSpaceChecker.java

---
+++ app/src/main/java/com/ekcapaper/racingar/data/LocationRequestSpaceChecker.java
@@ -0,0 +1,9 @@
+package com.ekcapaper.racingar.data;
+
+import android.content.Context;
+
+public class LocationRequestSpaceChecker extends LocationRequestSpace{
+    public LocationRequestSpaceChecker(Context context) {
+        super(context);
+    }
+}
