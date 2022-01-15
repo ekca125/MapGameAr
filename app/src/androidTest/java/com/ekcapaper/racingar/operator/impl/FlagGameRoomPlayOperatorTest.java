36d525cf43b05e38c9d542bd607a9e0d0c79da7e
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-15 14:18:09 +0900

    delete

---
+++ app/src/androidTest/java/com/ekcapaper/racingar/operator/impl/FlagGameRoomPlayOperatorTest.java
@@ -0,0 +1,24 @@
+package com.ekcapaper.racingar.operator.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlagGameRoomPlayOperatorTest {
+
+    @Test
+    public void isEnd() {
+    }
+
+    @Test
+    public void onMovePlayer() {
+    }
+
+    @Test
+    public void getPoint() {
+    }
+
+    @Test
+    public void getUnownedFlagList() {
+    }
+}
