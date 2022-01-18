565164a3bfd9a4906296b893735e4b70329eb5ea
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-18 11:21:38 +0900

    Login activity 개선

--- app/src/main/java/com/ekcapaper/racingar/activity/raar/LoginActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -72,7 +73,7 @@ public class LoginActivity extends AppCompatActivity implements ActivityInitiali
 
                     @Override
                     public void onFailure(Throwable t) {
-                        
+                        Toast.makeText(getApplicationContext(),"로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
