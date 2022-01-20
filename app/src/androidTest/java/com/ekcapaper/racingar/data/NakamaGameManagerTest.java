3b99c2a604d6a6be5c7ffea9b1a8fee76af63354
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-20 15:44:17 +0900

    commit

---
+++ app/src/androidTest/java/com/ekcapaper/racingar/data/NakamaGameManagerTest.java
@@ -0,0 +1,58 @@
+package com.ekcapaper.racingar.data;
+
+import static org.junit.Assert.*;
+
+import com.ekcapaper.racingar.stub.AccountStub;
+import com.ekcapaper.racingar.stub.ListenerStub;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NakamaGameManagerTest {
+    public static NakamaNetworkManager nakamaNetworkManager1;
+    public static NakamaNetworkManager nakamaNetworkManager2;
+
+    public static NakamaGameManager nakamaGameManager1;
+    public static NakamaGameManager nakamaGameManager2;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        nakamaNetworkManager1 = new NakamaNetworkManager();
+        nakamaNetworkManager2 = new NakamaNetworkManager();
+
+        nakamaNetworkManager1.loginEmailSync(AccountStub.ID,AccountStub.PASSWORD);
+        nakamaNetworkManager2.loginEmailSync(AccountStub.ID2,AccountStub.PASSWORD2);
+
+        nakamaGameManager1 = new NakamaGameManager(nakamaNetworkManager1);
+        nakamaGameManager2 = new NakamaGameManager(nakamaNetworkManager2);
+    }
+
+    @Test
+    public void createGameRoom() {
+        String roomName = RandomStringUtils.randomAlphabetic(10);
+        String roomDesc = "";
+
+        boolean result;
+        result = nakamaGameManager1.createGameRoom(roomName,roomDesc, ListenerStub.socketListenerEmpty);
+        assertTrue(result);
+
+        nakamaGameManager1.leaveGameRoom();
+    }
+
+    @Test
+    public void joinGameRoom(){
+        String roomName = RandomStringUtils.randomAlphabetic(10);
+        String roomDesc = "";
+
+        boolean result;
+        result = nakamaGameManager1.createGameRoom(roomName,roomDesc, ListenerStub.socketListenerEmpty);
+        assertTrue(result);
+        
+
+
+        nakamaGameManager1.leaveGameRoom();
+
+    }
+
+}
--- app/src/main/java/com/ekcapaper/racingar/data/NakamaGameManager.java
+++ app/src/main/java/com/ekcapaper/racingar/data/NakamaGameManager.java
@@ -2,6 +2,7 @@ package com.ekcapaper.racingar.data;
 
 import com.ekcapaper.racingar.operator.layer.GameRoomPlayOperator;
 import com.heroiclabs.nakama.Match;
+import com.heroiclabs.nakama.SocketListener;
 import com.heroiclabs.nakama.api.Group;
 
 import lombok.Getter;
@@ -11,7 +12,7 @@ public class NakamaGameManager{
     private NakamaNetworkManager nakamaNetworkManager;
     private Group roomGroup;
     private Match roomMatch;
-    private GameRoomPlayOperator roomOperator;
+    private SocketListener roomOperator;
 
     public NakamaGameManager(NakamaNetworkManager nakamaNetworkManager) {
         this.nakamaNetworkManager = nakamaNetworkManager;
@@ -24,14 +25,14 @@ public class NakamaGameManager{
         return roomOperator != null || roomGroup != null || roomMatch != null;
     }
 
-    public boolean createGameRoom(@NonNull String roomName, @NonNull String roomDesc, @NonNull GameRoomPlayOperator gameRoomPlayOperator){
+    public boolean createGameRoom(@NonNull String roomName, @NonNull String roomDesc, @NonNull SocketListener socketListener){
         if(isActive()){
             // 이미 활성화 된 상태라면 오류 발생
             throw new IllegalStateException();
         }
         // 그룹과 매치를 받아오기
         Group group = nakamaNetworkManager.createGroupSync(roomName,roomDesc);
-        Match match = nakamaNetworkManager.createMatchSync(gameRoomPlayOperator);
+        Match match = nakamaNetworkManager.createMatchSync(socketListener);
         // 정상 여부의 확인
         if(group == null || match == null){
             if(group != null){
@@ -43,10 +44,24 @@ public class NakamaGameManager{
             return false;
         }
         // 객체에 반영
-        this.roomOperator = gameRoomPlayOperator;
+        this.roomOperator = socketListener;
         this.roomGroup = group;
         this.roomMatch = match;
         return true;
     }
+
+    public void leaveGameRoom(){
+        if(!isActive()){
+            return;
+        }
+        // 떠나기
+        this.nakamaNetworkManager.leaveMatchSync(roomMatch.getMatchId());
+        this.nakamaNetworkManager.leaveGroupSync(roomGroup.getName());
+        // 초기화
+        this.roomGroup = null;
+        this.roomMatch = null;
+        this.roomOperator = null;
+    }
+
 
 }
