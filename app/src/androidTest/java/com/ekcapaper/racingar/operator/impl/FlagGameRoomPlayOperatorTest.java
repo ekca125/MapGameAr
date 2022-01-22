24fe84e3a17a78a2bff5e4f7aa7c54aeefc0ec12
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-22 14:14:39 +0900

    Create FlagGameRoomPlayOperatorTest.java

---
+++ app/src/androidTest/java/com/ekcapaper/racingar/operator/impl/FlagGameRoomPlayOperatorTest.java
@@ -0,0 +1,20 @@
+package com.ekcapaper.racingar.operator.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlagGameRoomPlayOperatorTest {
+
+    @Test
+    public void onMovePlayer() {
+    }
+
+    @Test
+    public void getPoint() {
+    }
+
+    @Test
+    public void getUnownedFlagList() {
+    }
+}
