ac027547f48a0f0033e7f71c0a5241eb0bc73fa6
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-01-24 11:41:39 +0900

    Update LobbyActivity.java

--- app/src/main/java/com/ekcapaper/racingar/activity/raar/LobbyActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/raar/LobbyActivity.java
@@ -3,8 +3,6 @@ package com.ekcapaper.racingar.activity.raar;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,17 +19,12 @@ import com.ekcapaper.racingar.adaptergame.AdapterLobby;
 import com.ekcapaper.racingar.data.LocationRequestSpace;
 import com.ekcapaper.racingar.data.NakamaGameManager;
 import com.ekcapaper.racingar.data.NakamaNetworkManager;
-import com.ekcapaper.racingar.data.NakamaRoomMetaDataManager;
 import com.ekcapaper.racingar.data.ThisApplication;
 import com.ekcapaper.racingar.modelgame.item.GameLobbyRoomItem;
 import com.ekcapaper.racingar.utils.Tools;
-import com.heroiclabs.nakama.api.Group;
-import com.heroiclabs.nakama.api.GroupList;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -49,7 +42,7 @@ public class LobbyActivity extends AppCompatActivity implements ActivityInitiali
     private AdapterLobby mAdapter;
     private List<GameLobbyRoomItem> items;
     // location service
-    private LocationRequestSpace refreshRequester;
+    private LocationRequestSpace locationRefresher;
     private Location currentLocation;
 
     @Override
@@ -61,13 +54,11 @@ public class LobbyActivity extends AppCompatActivity implements ActivityInitiali
 
     @Override
     public void initActivityField() {
+        // managers
         thisApplication = (ThisApplication) getApplicationContext();
         nakamaNetworkManager = thisApplication.getNakamaNetworkManager();
         nakamaGameManager = thisApplication.getNakamaGameManager();
-        //
-        mAdapter = null;
-        items = null;
-        //
+        // location
         currentLocation = null;
     }
 
@@ -79,6 +70,9 @@ public class LobbyActivity extends AppCompatActivity implements ActivityInitiali
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
 
+        mAdapter = null;
+        items = null;
+
         initToolbar();
     }
 
@@ -87,28 +81,62 @@ public class LobbyActivity extends AppCompatActivity implements ActivityInitiali
         button_new_room.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                refreshRequester.stop();
+                locationRefresher.stop();
                 Intent intent = new Intent(getApplicationContext(), GameRoomGenerateActivity.class);
                 startActivity(intent);
             }
         });
-        // item 초기화
-        List<GameLobbyRoomItem> items = new ArrayList<>();
-        items.add(new GameLobbyRoomItem("방의 정보를 불러오는 중..","",""));
-        updateLobbyView(items);
 
-        // 로비 갱신
-        refreshRequester = new LocationRequestSpace(this, new Consumer<Location>() {
+        setLobbyItemLocationMessage();
+        // 위치 갱신
+        locationRefresher = new LocationRequestSpace(this, new Consumer<Location>() {
             @Override
             public void accept(Location location) {
-                updateLocation(location);
-                updateLobbyData();
-                refreshRequester.stop();
+                currentLocation = location;
+                thisApplication.getExecutorService().submit(()->{
+                    updateLobbyData();
+                });
+                locationRefresher.stop();
             }
         });
-        refreshRequester.start();
+        locationRefresher.start();
     }
 
+    private void setLobbyItemLocationMessage(){
+        List<GameLobbyRoomItem> items = new ArrayList<>();
+        items.add(new GameLobbyRoomItem("위치를 불러오는 중..","",""));
+        updateLobbyView(items);
+    }
+
+
+    private void updateLobbyData(){
+        if(currentLocation == null){
+            // 반드시 위치를 가져온 이후에 업데이트의 용도로 접근되어야 한다.
+            throw new IllegalStateException();
+        }
+        List<GameLobbyRoomItem> items = new ArrayList<>();
+        items = nakamaNetworkManager.getAllGroupList().getGroupsList().stream()
+                .map(group->{
+                    return new GameLobbyRoomItem("test",group.getId(),"test");
+                })
+                .collect(Collectors.toList());
+        updateLobbyView(items);
+    }
+
+    private void updateLobbyView(List<GameLobbyRoomItem> gameLobbyRoomItems){
+        this.runOnUiThread(()->{
+            items = gameLobbyRoomItems;
+            mAdapter = new AdapterLobby(this, items);
+            recyclerView.setAdapter(mAdapter);
+            // on item list clicked
+            mAdapter.setOnItemClickListener(new AdapterLobby.OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, GameLobbyRoomItem obj, int position) {
+
+                }
+            });
+        });
+    }
 
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -131,12 +159,12 @@ public class LobbyActivity extends AppCompatActivity implements ActivityInitiali
         if (item.getItemId() == android.R.id.home) {
             finish();
         } else if (item.getItemId() == R.id.action_refresh) {
-            if(refreshRequester.isRunning()){
+            if(locationRefresher.isRunning()){
                 Toast.makeText(getApplicationContext(), "이미 방의 정보를 가져오는 중입니다.", Toast.LENGTH_SHORT).show();
             }
             else{
                 Toast.makeText(getApplicationContext(), "방의 정보를 다시 가져오고 있습니다.", Toast.LENGTH_SHORT).show();
-                refreshRequester.start();
+                locationRefresher.start();
             }
         } else {
             Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
@@ -144,76 +172,15 @@ public class LobbyActivity extends AppCompatActivity implements ActivityInitiali
         return super.onOptionsItemSelected(item);
     }
 
-    private void updateLocation(Location location){
-        this.currentLocation = location;
-    }
-
-    private void updateLobbyData(){
-        if(currentLocation == null){
-            // 반드시 위치를 가져온 이후에 업데이트의 용도로 접근되어야 한다.
-            throw new IllegalStateException();
-        }
-        List<GameLobbyRoomItem> items = new ArrayList<>();
-        items.add(new GameLobbyRoomItem("test","",""));
-        items.add(new GameLobbyRoomItem("test","",""));
-        items.add(new GameLobbyRoomItem("test","",""));
-        items.add(new GameLobbyRoomItem("test","",""));
-        items.add(new GameLobbyRoomItem("test","",""));
-    }
-
-    private void updateLobbyView(List<GameLobbyRoomItem> gameLobbyRoomItems){
-        items = gameLobbyRoomItems;
-        mAdapter = new AdapterLobby(this, items);
-        recyclerView.setAdapter(mAdapter);
-        // on item list clicked
-        mAdapter.setOnItemClickListener(new AdapterLobby.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, GameLobbyRoomItem obj, int position) {
-
-            }
-        });
-    }
-
-/*
-    private void updateLobbyData(Location location) {
-        List<GameLobbyRoomItem> items = new ArrayList<>();
-        try {
-            GroupList groupList = nakamaNetworkManager.getAllGroupList();
-            groupList.getGroupsList().stream().forEach((group -> {
-                Log.d("group",group.getId());
-            }));
-
-            items = groupList.getGroupsList().stream()
-                    .map()
-                    .collect(Collectors.toList());
-        } catch (NullPointerException ignored) {
-            items.add(new GameLobbyRoomItem("현재 열려있는 방이 없습니다.", "",""));
-        }
-        this.items = items;
-        runOnUiThread(()->{
-            mAdapter = new AdapterLobby(this, this.items);
-            recyclerView.setAdapter(mAdapter);
-            // on item list clicked
-            mAdapter.setOnItemClickListener(new AdapterLobby.OnItemClickListener() {
-                @Override
-                public void onItemClick(View view, GameLobbyRoomItem obj, int position) {
-                    Toast.makeText(getApplicationContext(), "미구현된 내용입니다.", Toast.LENGTH_SHORT).show();
-                }
-            });
-        });
-    }
-
- */
-
     @Override
     protected void onResume() {
         super.onResume();
-        refreshRequester.start();
+        locationRefresher.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        refreshRequester.stop();
+        locationRefresher.stop();
     }
 }
