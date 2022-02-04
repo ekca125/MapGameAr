601560566e0d5c9ad8ecccc70288ef0a9f7328e1
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-02-04 10:57:07 +0900

    Create TagGameRoomClient.java

---
+++ app/src/main/java/com/ekcapaper/mapgamear/operator/TagGameRoomClient.java
@@ -0,0 +1,11 @@
+package com.ekcapaper.mapgamear.operator;
+
+import com.ekcapaper.mapgamear.nakama.NakamaNetworkManager;
+
+public class TagGameRoomClient extends GameRoomClient{
+    public TagGameRoomClient(NakamaNetworkManager nakamaNetworkManager) {
+        super(nakamaNetworkManager);
+    }
+
+    
+}
