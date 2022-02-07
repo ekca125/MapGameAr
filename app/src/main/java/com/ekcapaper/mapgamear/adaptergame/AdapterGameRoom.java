c099f87eb30e29e802babb5171e6e53a149db450
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-02-07 16:43:26 +0900

    adapter

--- app/src/main/java/com/ekcapaper/mapgamear/adaptergame/AdapterGameRoom.java
+++ app/src/main/java/com/ekcapaper/mapgamear/adaptergame/AdapterGameRoom.java
@@ -14,36 +14,19 @@ import com.ekcapaper.mapgamear.modelgame.item.GameRoomInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AdapterGameRoom extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class AdapterGameRoom extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<GameRoomInfo> items = new ArrayList<>();
 
-    private Context ctx;
+    private final Context ctx;
     private AdapterGameRoom.OnItemClickListener mOnItemClickListener;
-
-    public interface OnItemClickListener {
-        void onItemClick(View view, GameRoomInfo obj, int position);
-    }
-
-    public void setOnItemClickListener(final AdapterGameRoom.OnItemClickListener mItemClickListener) {
-        this.mOnItemClickListener = mItemClickListener;
-    }
 
     public AdapterGameRoom(Context context, List<GameRoomInfo> items) {
         this.items = items;
         ctx = context;
     }
 
-    public class OriginalViewHolder extends RecyclerView.ViewHolder {
-        public TextView user_name;
-        public TextView user_id;
-        public View lyt_parent;
-
-        public OriginalViewHolder(View v) {
-            super(v);
-            user_name = (TextView) v.findViewById(R.id.user_name);
-            user_id = (TextView) v.findViewById(R.id.user_id);
-            lyt_parent = (View) v.findViewById(R.id.lyt_parent);
-        }
+    public void setOnItemClickListener(final AdapterGameRoom.OnItemClickListener mItemClickListener) {
+        this.mOnItemClickListener = mItemClickListener;
     }
 
     @Override
@@ -77,5 +60,22 @@ public class AdapterGameRoom extends RecyclerView.Adapter<RecyclerView.ViewHolde
     @Override
     public int getItemCount() {
         return items.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, GameRoomInfo obj, int position);
+    }
+
+    public class OriginalViewHolder extends RecyclerView.ViewHolder {
+        public TextView user_name;
+        public TextView user_id;
+        public View lyt_parent;
+
+        public OriginalViewHolder(View v) {
+            super(v);
+            user_name = (TextView) v.findViewById(R.id.user_name);
+            user_id = (TextView) v.findViewById(R.id.user_id);
+            lyt_parent = (View) v.findViewById(R.id.lyt_parent);
+        }
     }
 }
