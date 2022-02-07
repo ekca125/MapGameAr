f5052f0a4cc3185ca018c78b0fb280b055190873
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-02-07 16:46:57 +0900

    message

--- app/src/main/java/com/ekcapaper/mapgamear/network/GameMessageStart.java
+++ app/src/main/java/com/ekcapaper/mapgamear/network/GameMessageStart.java
@@ -1,7 +1,5 @@
 package com.ekcapaper.mapgamear.network;
 
-import com.google.gson.Gson;
-
 public class GameMessageStart extends GameMessage {
     public GameMessageStart() {
         super(GameMessageOpCode.GAME_START);
