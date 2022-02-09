02b138d36aaef2c8e7c62f86d6456bdb9653c495
ekca125 <63381869+ekca125@users.noreply.github.com>
2022-02-09 15:59:45 +0900

    Update GameMapActivity.java

--- app/src/main/java/com/ekcapaper/mapgamear/activity/raar/GameMapActivity.java
+++ app/src/main/java/com/ekcapaper/mapgamear/activity/raar/GameMapActivity.java
@@ -206,7 +206,6 @@ public class GameMapActivity extends AppCompatActivity {
                         showDialogVictory();
                     }
                 }
-                finish();
             });
         });
 
